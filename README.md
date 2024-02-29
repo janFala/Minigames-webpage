b5e085a9eb9757c0f960f5603f43802212a6d21c
janfala <janfalarowski@web.de>
2024-02-29 19:47:02 +0100

    finished second minigame

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Second Javascript project - Collection of Minigames
-## First Day
+# Second Javascript project - Small Collection of Minigames
+
+## Day 1
+
 After watching, learning and practicing more javascript, html and css, I decided to create this project
+
 - it's my second day of learning these languages
 
-### Thoughts after first day
+### Thoughts after day 1
+
 - My css skills are not the yellow from the egg (I get the basics, but suck at layout)
 - I stole code for styling my buttons (but they have nice animations now)
 - Javascript feels more familiar to use the more I learn (today I learned about objects (among other things), let's see if there will be classes)
@@ -11,3 +15,27 @@ After watching, learning and practicing more javascript, html and css, I decided
 - I still suck at naming variables (always have been)
 - The js code is of better quality then yesterday, but the html and css is really not great
 - I only finished the first game for now, tomorrow before learning, I will finish the second game. (after which I will probably do different projects)
+
+## Day 2
+
+### Before starting on Day 2
+
+- I wanna start doing a codecamp certificate next, instead of just learning through youtube videos and other online ressources, therefore I will not put much more effort into this project for now
+
+### After finisishing project for now
+
+- Im pretty happy with how it turned out
+- It look's good in my opinion (haven't tested it on mobile yet)
+- Code and variable names are still not great (In OOP it's way easier to keep everything organized, you should probably split script files into multiple ones if they get larger, or something like that. let's see what i will learn about that topic)
+- Spent most time at Css today, since coinflip was really easy to code
+- I definitly wan't to look into async functions and promises, since I user setTimeout() and that leads to multiple alerts being chained after each other if buttons are spammed
+- Also fixing css is way easier if you have the final layout in the markdown before starting with js. You should always do as much as possible of the .html
+
+### What's next for me
+
+- I will take a look at the js course of freecodecamp for today
+- Tomorrow I will do another project, let's see what it will be
+
+## Final thought
+
+- Not impressive project in terms of logic or the css, but I feel more familiar with the overall web-environment now
--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,6 @@ const userStats = JSON.parse(localStorage.getItem("userStats")) || {
   },
   coinflip: {
     wins: 0,
-    ties: 0,
     losses: 0,
   },
 };
@@ -20,10 +19,22 @@ function rockPaperScissors(choice) {
   updateUserStats(gameOutcome, "rockPaperScissors");
 
   // display gameResult
-  displayGameResult(userChoice, computerChoice, gameOutcome);
+  const computerPickDisplay = document.getElementById("computerPick");
+  const userPickDisplay = document.getElementById("userPick");
+  displayGameResult(userChoice, computerChoice, computerPickDisplay, userPickDisplay);
 
   // store stats in local storage
   storeUserStats();
+
+  alertGameResult(gameOutcome, "rockPaperScissors");
+  resetUi(computerPickDisplay, userPickDisplay);
+}
+
+function resetUi(element1, element2) {
+  setTimeout(() => {
+    element1.textContent = "";
+    element2.textContent = "";
+  }, 2000);
 }
 
 // let computer picks rock paper or scissor
@@ -75,31 +86,74 @@ function storeUserStats() {
   localStorage.setItem("userStats", JSON.stringify(userStats));
 }
 
-function displayGameResult(userChoice, computerChoice, gameOutcome) {
-  const computerPick = document.getElementById("computerPick");
-  const userPick = document.getElementById("userPick");
+function displayGameResult(userChoice, computerChoice, computerPickElement, userPickElement) {
+  userPickElement.textContent = userChoice;
 
-  userPick.textContent = userChoice;
   setTimeout(() => {
-    computerPick.textContent = computerChoice;
+    computerPickElement.textContent = computerChoice;
   }, 1250);
-  alertGameResult(gameOutcome);
 }
 
-function alertGameResult(gameOutcome) {
+function alertGameResult(gameOutcome, gamemode) {
   let message = "";
   switch (gameOutcome) {
     case "win":
       message = "You Won!";
       break;
     case "lose":
-      message = "You lost :(";
+      message = "You Lost :(";
       break;
     case "tie":
-      message = "You tied!";
+      message = "You Tied!";
   }
-  message = `3. Get the result: \n${message}\nYour stats for this gamemode:\nW: ${userStats.rockPaperScissors.wins}, L: ${userStats.rockPaperScissors.losses}, T: ${userStats.rockPaperScissors.ties}`;
+  if (gamemode === "rockPaperScissors") {
+    message = `3. Get the result: \n${message}\nYour stats for this gamemode:\nW: ${userStats.rockPaperScissors.wins}, L: ${userStats.rockPaperScissors.losses}, T: ${userStats.rockPaperScissors.ties}`;
+  } else {
+    message = `3. Get the result: \n${message}\nYour stats for this gamemode:\nW: ${userStats.coinflip.wins}, L: ${userStats.coinflip.losses}`;
+  }
   setTimeout(() => {
     alert(message);
   }, 2000);
+}
+
+// coinflip game
+function playCoinflip(userChoice) {
+  let coinResult = flipCoin(userChoice);
+  updateUserStats(coinResult[1], "coinflip");
+
+  // display game info's
+  const computerDisplay = document.getElementById("computerPick2");
+  const userDisplay = document.getElementById("userPick2");
+  displayGameResult(userChoice, coinResult[0], computerDisplay, userDisplay);
+
+  storeUserStats(coinResult[1], "coinflip");
+
+  alertGameResult(coinResult[1], "coinflip");
+
+  resetUi(computerDisplay, userDisplay);
+}
+
+// returns [Winning coinside; result]
+function flipCoin(userChoice) {
+  let coinResult = ["", ""];
+  Math.random() < 0.5 ? (coinResult[0] = "tails") : (coinResult[0] = "heads");
+
+  coinResult[0] === userChoice ? (coinResult[1] = "win") : (coinResult[1] = "lose");
+  return coinResult;
+}
+
+function resetRockPaperScissors() {
+  userStats.rockPaperScissors.wins = 0;
+  userStats.rockPaperScissors.losses = 0;
+  userStats.rockPaperScissors.ties = 0;
+}
+
+function resetCoinflip() {
+  userStats.coinflip.wins = 0;
+  userStats.coinflip.losses = 0;
+}
+
+function resetAll() {
+  resetRockPaperScissors();
+  resetCoinflip();
 }
